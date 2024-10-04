78206ac6f0e6210f2d0c2d98afde539bf75c6bd0
ferinmtk <mut2400@byui.edu>
2024-10-04 18:54:06 +0300

    awow change

---
+++ scripts/getdates.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', (event) => {
+        alert(`Navigating to ${event.target.innerText}`);
+    });
+});
+
+
+document.getElementById('year').textContent = new Date().getFullYear();
+const lastModifiedDate = document.lastModified; 
+document.getElementById('lastModified').textContent = lastModifiedDate; 
